2588ac5946f6856828026dd9efe3adbc607bae16
Duncan MacLennan <6733@holbertonstudents.com>
2023-09-03 22:21:15 -0700

    update betty

--- main.c
+++ main.c
@@ -76,24 +76,7 @@ int is_valid_integer(const char *str)
 	}
 	return (1);
 }
-/**
- * free_stack - frees memory
- * @stack: the stack
- */
-void free_stack(stack_t **stack)
-{
-	stack_t *current_node;
-	stack_t *next_node;
 
-	current_node = *stack;
-	while (current_node != NULL)
-	{
-		next_node = current_node->next;
-		free(current_node);
-		current_node = next_node;
-	}
-	*stack = NULL;
-}
 /**
  * handle_line - does the monty
  * @line: the line
